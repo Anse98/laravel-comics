3c8d039f95e453ca353881e370306492db04f498
Anse98 <lorenzo.anselmi98.com>
2023-12-11 16:38:27 +0100

    ADD-file layout generale, con partials header e footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,71 @@
+<footer>
+    <section>
+            <div class="top-footer">
+                <div class="container">
+                    <div class="menu-list d-flex">
+                        <div class="dc-comics-shop">
+
+                            <div class="dc-comics">
+                                <h3>DC COMICS</h3>
+                                <ul>
+                                    <li>Characters</li>
+                                    <li>Comics</li>
+                                    <li>Movies</li>
+                                    <li>TV</li>
+                                    <li>Games</li>
+                                    <li>Videos</li>
+                                    <li>News</li>
+                                </ul>
+                            </div>
+
+                            <div class="shop">
+                                <h3>SHOP</h3>
+                                <ul>
+                                    <li>Shop DC</li>
+                                    <li>Shop DC Collectibles</li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                        <div class="dc">
+                            <h3>DC</h3>
+                            <ul>
+                                <li>Terms Of Use</li>
+                                <li>Privacy policy</li>
+                                <li>Ad Choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                            </ul>
+                        </div>
+
+
+                        <div class="sites">
+                            <h3>SITES</h3>
+                            <ul>
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <p>All Site Content TM and @ 2020 DC Enterteinment, unless otherwise noted here. All rights reserved.</p>
+                </div>
+            </div>
+
+            <div class="bottom-footer">
+                <div class="container">
+
+                </div>
+            </div>
+    </section>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,37 @@
+
+<header class="main-header">
+    <div class="top-header">
+        <div class="container">
+            <div class="text d-flex justify-content-end gap-3 align-items-center">
+                <p>DC POWER VISA@</p>
+                <p>ADDITIONAL DC SITES</p>
+            </div>
+        </div>
+    </div>
+    <div class="bottom-header">
+        <div class="container">
+            <div class="nav-list d-flex gap-4 justify-content-between flex-wrap">
+                <div class="logo">
+                    logo
+                </div>
+    
+                <ul class="d-flex gap-3 flex-wrap">
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+    
+                <div class="search">
+                    search
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
